1af5379a0f56b48113bf1087f1402737b68342f9
emanueleromito <1dromito.emanuele@gmail.com>
2022-06-25 20:13:22 +0200

    new commenti

--- src/gestione_gadget.h
+++ src/gestione_gadget.h
@@ -16,6 +16,8 @@
  *
  * Funzione incaricata di gestire l'input di dati riguardanti i gadget.
  * @param[OTPUT] esito: int
+ * @retval "1" Se l'inserimento e' stato completato.
+ * @retval "0" Se si e' presentato un errore.
  */
 int registrazione_gadget();
 /**@brief
@@ -29,7 +31,7 @@ void modifica_gadget_utente(int scelta);
  * Procedura che a seconda del valore del parametro "scelta" inserito da tastiera, chiama la funzione ricerca_gadget nelle seguenti modalita': ricerca per nome(0), ricerca per codice(1), ricerca per colore(2).
  */
 void effettua_ricerca();
-int modifica_quant_prezz_venduti_gadget(char gadget[CARATTERI],float valore, int scelta);
+
 
 
 #endif /* GESTIONE_GADGET_H_ */
--- src/gestione_utente.h
+++ src/gestione_utente.h
@@ -14,10 +14,32 @@
 #include <stdlib.h>
 
 
-
+/**@brief
+ *
+ * Funzione incaricata di gestire l'input di dati riguardanti i gadget.
+ * @param[OUTPUT] esito: int
+ * @retval "1" Se l'inserimento e' stato completato.
+ * @retval "0" Se si e' presentato un errore.
+ */
 int registrazione_cliente();
+/**@brief
+ *
+ * Funzione incaricata di gestire il log-in utente. Se le credenziali date in input sono equivalenti a quelle dell'admin allora verra' restituito il valore "2", altrimenti utilizzando la funzione "restituisci_cliente", si controlla se e' presente un cliente nel file "file_clienti" con determinate credenziali. Se le credenziali sono corrette, verra' restituito il valore "1".
+ * @param[INPUT] username: char[50]
+ * @param[INPUT] pass: char[50]
+ * @param[OUTPUT] esito_accesso: int
+ * @retval "2" Se l'inserimento e' stato completato da gestore.
+ * @retval "1" Se l'inserimento e' stato completato da cliente.
+ * @retval "0" Se si e' presentato un errore.
+ */
 int accesso_cliente(char username[CARATTERI], char pass[CARATTERI]);
-void modifica_cliente_RagSoc(char username_corrente[CARATTERI],char rag[CARATTERI]);
+
+/**@brief
+ *
+ * Procedura incaricata di gestire la modifica utente. A seconda del valore del parametro "scelta" verranno modificati diversi parametri: p.iva (1), rag. sociale (2), citta' (3). Tramite la stringa "username_corrente" data in input siamo in grado, tramite la funzione "restituisci_cliente" di risalire al cliente da modificare. Una volta ottenuta la struct del cliente da modificare verra' modificato il parametro desiderato in una struct di appoggio e verra' cancellato il record dal file, in seguito verra' dunque caricato il record con la struct aggiornata.
+ * @param[INPUT] username_corrente: char[50]
+ * @param[INPUT] scelta: int
+ */
 void modifica_cliente(char username_corrente[CARATTERI],int scelta);
 
 
